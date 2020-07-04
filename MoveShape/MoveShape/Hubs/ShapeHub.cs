ae014589df006c3db4f210ca526e58125c75476e
m.girgin <m.girgin@groupama.com.tr>
2020-07-04 17:10:41 +0300

    Cahnge color of object

--- MoveShape/MoveShape/Hubs/ShapeHub.cs
+++ MoveShape/MoveShape/Hubs/ShapeHub.cs
@@ -5,9 +5,9 @@ namespace MoveShape.Hubs
 {
     public class ShapeHub : Hub
     {
-        public async Task MoveShape(int x , int y)
+        public async Task MoveShape(string id, int x , int y,string color)
         {
-            await Clients.Others.SendAsync("shapeMoved", x, y);
+            await Clients.Others.SendAsync("shapeMoved", id, x, y,color);
         }
     }
 }
