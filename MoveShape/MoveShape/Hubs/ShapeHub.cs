4e0806bcdd46b6fc10579e6a3db4cd553b6fda19
m.girgin <m.girgin@groupama.com.tr>
2020-07-04 21:59:21 +0300

    changes of fire

--- MoveShape/MoveShape/Hubs/ShapeHub.cs
+++ MoveShape/MoveShape/Hubs/ShapeHub.cs
@@ -5,9 +5,14 @@ namespace MoveShape.Hubs
 {
     public class ShapeHub : Hub
     {
-        public async Task MoveShape(string id, int x , int y,string color)
+        public async Task MoveShape(string id, int x , int y,string color, string userchar)
         {
-            await Clients.Others.SendAsync("shapeMoved", id, x, y,color);
+            await Clients.Others.SendAsync("shapeMoved", id, x, y,color, userchar);
+        }
+
+        public async Task FireShape(string id, int x, int y)
+        {
+            await Clients.Others.SendAsync("shapeFired", id, x, y);
         }
     }
 }
